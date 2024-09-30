a7ee095d8a434008d5f95d116b61a4485bce6b12
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2024-09-30 08:11:48 +0300

    Fix typo

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,6 @@ export * from "./order/status";
 export * from "./icons";
 export * from "./order/actions";
 export * from "./dashboard";
-export * from "./map";
 export * from "./order";
 export * from "./paginationTotal";
 export * from "./customer/userStatus";
