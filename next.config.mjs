9a123533e1597775194ad1e4fd8ee11f6837b3df
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2024-10-01 18:51:19 +0300

    Add brotli compression

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@refinedev/antd"],
+  output: "standalone",
+  compress: false,
 };
 
 export default nextConfig;
